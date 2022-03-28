ccb50dbdd079ab520bca392c37c21f2092eba8f3
Milan Tasevski <tasevski.milan22@hotmail.com>
2022-03-28 19:47:12 +0200

    Initial commit

---
+++ src/test/java/mk/ukim/finki/emt/books/BooksApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.emt.books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
